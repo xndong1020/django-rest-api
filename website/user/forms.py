abb43635213a11b709db271a2d33ac157c42f810
Jeremy <isdance2004@hotmail.com>
2018-10-09 23:07:29 +1100

    Add form validation in user signup form for checking if username is taken

--- website/user/forms.py
+++ website/user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate
 
 
 class UserSignupForm(forms.ModelForm):
@@ -21,7 +22,16 @@ class UserSignupForm(forms.ModelForm):
         if not email:
             raise forms.ValidationError('email is required!')
 
+        # if User.objects.exclude(pk=self.instance.pk).filter(username=username).exists():
+        #     raise forms.ValidationError('This user already exists!')
+
         return super(UserSignupForm, self).clean(*args, **kwargs)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.exclude(pk=self.instance.pk).filter(username=username).exists():
+            raise forms.ValidationError(u'Username "%s" is already in use.' % username)
+        return username
 
     def save(self):
         #  Saving with commit=False gets you a model object, then you can add your extra data and save it.
